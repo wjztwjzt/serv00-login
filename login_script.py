b5936d5e282b754f02181e1b7a2b977ca1e6494d
wjztwjzt <123343489+wjztwjzt@users.noreply.github.com>
2024-10-21 00:04:44 +0800

    Update login_script.py

--- login_script.py
+++ login_script.py
@@ -10,7 +10,8 @@ import os
 # 从环境变量中获取 Telegram Bot Token 和 Chat ID
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-
+dai = os.getenv('ACCOUNTS_JSON')
+print(dai)
 def format_to_iso(date):
     return date.strftime('%Y-%m-%d %H:%M:%S')
 
